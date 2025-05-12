bf3db46a56dd16b21ca5ae32bd6de065e7b408be
chelik10228 <dxxeas@gmail.com>
2025-05-12 18:03:12 +0200

    Add trap

--- cli.py
+++ cli.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-print("\033[31mtrapped\033[0m at 0x000001");
+print("\033[31mtrapped\033[0m");
 while (True):
 	com = input("! ");
 	if not com:
@@ -9,9 +9,13 @@ while (True):
 		print("LastOS CLI Command Help 1/1 Page");
 		print("  h - help");
 		print("  t - show trap codes");
+		print("  c - clear screen");
 		print("  q - exit from cli");
 	elif com == "t":
 		print("0x000001 - command trap test or start cli");
+		print("0x000002 - error in python ZeroDivisionError");
+	elif com == "c":
+		print("\033[H\033[2J");
 	elif com == "q":
 		exit();
 	else:
